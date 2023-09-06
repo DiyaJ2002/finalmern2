bd89e611ac5fc3d471802b5e6eaf9ef0d92f5052
DiyaJ2002 <102916611+DiyaJ2002@users.noreply.github.com>
2023-09-06 18:47:39 +0530

    Update productAction.js

--- frontend1/src/actions/productAction.js
+++ frontend1/src/actions/productAction.js
@@ -16,7 +16,7 @@ export const getProduct =
     try {
       dispatch({ type: ALL_PRODUCT_REQUEST });
 
-      const { data } = await axios.get("api/v1/products");
+      const { data } = await axios.get("https://finalmern2.vercel.app/products");
 
       dispatch({
         type: ALL_PRODUCT_SUCCESS,
@@ -34,7 +34,7 @@ export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`api/v1/${id}`);
+    const { data } = await axios.get(`https://finalmern2.vercel.app/${id}`);
 
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
