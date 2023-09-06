0a7ec47c2c68cc43b5a96968fb6fd69cf86e5104
DiyaJ2002 <102916611+DiyaJ2002@users.noreply.github.com>
2023-09-06 18:41:50 +0530

    Update userAction.js

--- frontend1/src/actions/userAction.js
+++ frontend1/src/actions/userAction.js
@@ -18,7 +18,7 @@ export const login = (email, password) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.post(
-      `https://finalmern3-backend.vercel.app/login`,
+      `api/v1/login`,
       { email, password },
       config
     );
@@ -36,7 +36,7 @@ export const register = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.post(`https://finalmern3-backend.vercel.app/register`, userData, config);
+    const { data } = await axios.post(`api/v1/register`, userData, config);
 
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
